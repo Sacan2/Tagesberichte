d482503e006480150352093617ace7c02f005bee
sacan <serhad1@outlook.de>
2023-08-31 08:26:37 +0200

    tESTE

--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class MyFrame {
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    JTextArea taskInput = new JTextArea();
+    JScrollPane scrollBar = new JScrollPane(taskInput);
+    JLabel textForTask = new JLabel();
+    JLabel textForTime = new JLabel();
+
+
+    MyFrame() {
+        this.setTitle("Tagesberichte");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        scrollBar.setBounds(10, 40, 280, 180);
+        taskInput.setLineWrap(true);
+
+
+        textForTask.setText("Erledigte Tätigkeiten");
+        textForTask.setBounds(35, 17, 200, 30);
+
+        textForTime.setText("Benötigte Zeit");
+        textForTime.setBounds(355, 17, 200, 30);
+
+
+
+        this.add(textForTime);
+        this.add(textForTask);
+        this.add(scrollBar);
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
 }
