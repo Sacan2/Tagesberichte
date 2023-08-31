d1c6fa9065c85cd3a69aaab865e98cad6354413c
sacan <serhad1@outlook.de>
2023-08-31 13:34:43 +0200

    Die zahlen von 06:00 bis 06:55 gehen

--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
+import java.util.Objects;
 
 public class MyFrame extends JFrame {
     JTextArea taskInput = new JTextArea();
     JScrollPane scrollBar = new JScrollPane(taskInput);
     JLabel textForTask = new JLabel();
     JLabel textForTime = new JLabel();
-
+    JComboBox<String> timeDropDown = new JComboBox<String>();
 
     MyFrame() {
         this.setTitle("Tagesberichte");
@@ -15,19 +16,38 @@ public class MyFrame extends JFrame {
         scrollBar.setBounds(10, 40, 280, 180);
         taskInput.setLineWrap(true);
 
-
         textForTask.setText("Erledigte Tätigkeiten");
         textForTask.setBounds(35, 17, 200, 30);
 
         textForTime.setText("Benötigte Zeit");
         textForTime.setBounds(355, 17, 200, 30);
 
+        timeDropDown.setEditable(true);
+        timeDropDown.setBounds(350,50,100,20);
+        for (int i = 6,j=0; i < 18 && j < 60; i++,j +=5) {
+            String iFormatierung = String.format("%02d", i);
+            String jFormatierung = String.format("%02d", j);
+            int counter = 6;
+            String zero = "0";
 
 
+                if (Objects.equals(iFormatierung, "06")){
+                    timeDropDown.addItem(iFormatierung + ":"+jFormatierung);
+                    i--;
+                }
+
+        }
+
+
+
+
+
+        this.add(timeDropDown);
         this.add(textForTime);
         this.add(textForTask);
         this.add(scrollBar);
         this.setSize(500, 500);
         this.setVisible(true);
+        }
     }
-}
+
