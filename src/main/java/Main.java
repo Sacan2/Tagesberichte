93b2f7c4e812e43e973952dc2a3e933daf184bc9
sacan <serhad1@outlook.de>
2023-09-01 13:08:18 +0200

    Das erste Layout ist fertig

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,6 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        for (int hour = 6; hour <= 18; hour++) {
-            for (int minute = 0; minute < 60; minute += 5) {
-                if (hour == 18 && minute > 0) {
-                    break; // Stop at 18:00
-                }
-                Date time = new Date(0, 0, 0, hour, minute);
-                System.out.println(time);
-            }
-        }
         new MyFrame();
     }
 }
--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -3,13 +3,17 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MyFrame extends JFrame {
+    String[] daysArray = {"Montag","Dienstag","Mittwoch","Donnerstag","Freitag"};
     JTextArea taskInput = new JTextArea();
     JScrollPane scrollBar = new JScrollPane(taskInput);
     JLabel textForTask = new JLabel();
     JLabel textForTime = new JLabel();
+    JLabel textForDay = new JLabel();
     JComboBox<String> timeStart = new JComboBox<String>();
     JComboBox<String> timeEnd = new JComboBox<String>();
-
+    JComboBox<String> dayChoice = new JComboBox<String>(daysArray);
+    JButton createButton = new JButton("Excel erstellen");
+    JButton rowButton = new JButton("Spalte beschreiben");
     MyFrame() {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
         this.setTitle("Tagesberichte");
@@ -25,11 +29,23 @@ public class MyFrame extends JFrame {
         textForTime.setText("Benötigte Zeit");
         textForTime.setBounds(355, 17, 200, 30);
 
+        textForDay.setText("Tag");
+        textForDay.setBounds(355, 127, 200, 30);
+
         timeStart.setEditable(true);
         timeStart.setBounds(350, 50, 100, 20);
 
         timeEnd.setEditable(true);
         timeEnd.setBounds(350,100,100,20);
+
+        dayChoice.setEditable(false);
+        dayChoice.setBounds(350,160,100,20);
+
+
+        rowButton.setBounds(280,300,150,40);
+
+
+        createButton.setBounds(40,300,150,40);
 
         for (int hour = 6; hour <= 18; hour++) {
             for (int minute = 0; minute < 60; minute += 5) {
@@ -40,13 +56,15 @@ public class MyFrame extends JFrame {
                 timeStart.addItem(sdf.format(time));
                 timeEnd.addItem(sdf.format(time));
             }
-
-    }
-
+        }
+        this.add(rowButton);
+        this.add(dayChoice);
         this.add(timeStart);
         this.add(timeEnd);
+        this.add(createButton);
         this.add(textForTime);
         this.add(textForTask);
+        this.add(textForDay);
         this.add(scrollBar);
         this.setSize(500, 500);
         this.setVisible(true);
